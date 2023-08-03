a7430f6ac109c29f2c31fadb617690c656c08daa
Prateek Samaiya <prateek.samaiya@myntra.com>
2023-08-03 18:51:17 +0530

    made text size dynamic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,9 +15,9 @@ return(
         <CarouselItem/> 
         </Col>
         <Col className="p-5  border border-5 d-flex flex-column justify-content-around">
-        <p className="fs-2  message-1">Want to a perfect gift for an occassion or impress your wife, family, friend or a collegue with a perfect gift ? </p>
-        <p className="fs-1 message-2"> We are here to help !!</p>
-        <p className="fs-2  message-2"><b>Studio N </b> brings meticullously curated designs to lit your special moments.</p>
+        <p className="message-1">Want to a perfect gift for an occassion or impress your wife, family, friend or a collegue with a perfect gift ? </p>
+        <p className="message-1"> We are here to help !!</p>
+        <p className="message-1"><b>Studio N </b> brings meticullously curated designs to lit your special moments.</p>
         </Col>
         </Row>
         <h1 className="py-5">Products</h1>
