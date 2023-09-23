473f072821f5ad92860a7bcf3d30091986ce218f
Prateek Samaiya <prateek.samaiya@myntra.com>
2023-09-24 01:12:46 +0530

    changed title logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,7 +10,7 @@ function NavScrollExample() {
     <Navbar  expand="lg" className='pb-0'>
       <Container fluid className='pe-5 d-flex flex-row border'>
         <Navbar.Toggle />
-        <img src='mainLogo.PNG' width={120}></img>
+        <img src='nstudioLogo.png' width={120}></img>
         <Navbar.Collapse  >
           <Nav className='mx-auto'>
             <Nav.Link as={HashLink} className='mx-3' to="/">Home</Nav.Link>
